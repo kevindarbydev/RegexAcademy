cef21b217b215352c25d90f749bb0185c54c89b8
MattReda11 <103293959+MattReda11@users.noreply.github.com>
2023-01-18 11:05:32 -0500

    Users and Password Encryptor created

---
+++ RegexAcademy/PasswordEncryptor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexAcademy
+{
+    public class PasswordEncryptor
+    {
+
+        public static string EncryptPassword(string password)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            md5.ComputeHash(ASCIIEncoding.UTF8.GetBytes(password));
+
+            byte[] result = md5.Hash;
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < result.Length; i++)
+            {
+                sb.Append(result[i].ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ RegexAcademy/Users.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexAcademy
+{
+    public class Users
+    {
+
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        protected virtual string PasswordStored { get; set; }
+
+
+        [NotMapped]
+        public string Password 
+        { 
+            get { return PasswordStored; } 
+            set
+            {
+                PasswordStored = PasswordEncryptor.EncryptPassword(value);
+            } 
+        }
+
+    }
+}
