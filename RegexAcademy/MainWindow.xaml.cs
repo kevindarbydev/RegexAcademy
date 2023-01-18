3c9e82fba6c70c54629af3a62fb924975bae7a00
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-01-18 11:36:50 -0500

    stack panel buttons

--- RegexAcademy/MainWindow.xaml.cs
+++ RegexAcademy/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RegexAcademy
 {
@@ -23,6 +12,36 @@ namespace RegexAcademy
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StackPanelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            switch (button.Name)
+            {
+                case "Students":
+                    //ContentFrame.Content = new TextBlock { Text = "Button 1 clicked" };
+                    ContentFrame.Source = new Uri("StudentWindow.xaml", UriKind.Relative);
+
+                    break;
+                case "Teachers":
+                    ContentFrame.Source = new Uri("TeacherWindow.xaml", UriKind.Relative);
+                    break;
+                case "Courses":
+                    ContentFrame.Source = new Uri("CoursesWindow.xaml", UriKind.Relative);
+                    break;
+                case "StudentCourses":
+                    ContentFrame.Source = new Uri("StudentCoursesWindow.xaml", UriKind.Relative);
+                    break;
+            }
+        }
+    }
+
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+
         }
     }
 }
