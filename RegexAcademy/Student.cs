d24daca13e99b457ef54250add853405d4de396e
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-01-18 10:36:55 -0500

    first iteration of Student class

---
+++ RegexAcademy/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegexAcademy
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
+        public Student()
+        {
+
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        private string _firstName;
+
+        [Required]
+        [StringLength(40)]
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (value.Length < 1 || value.Length > 40) // needs more validation, ie. check for special characters
+                {
+                    throw new ArgumentException("First name length should be 1-40 characters long");
+                }
+                _firstName = value;
+            }
+        }
+
+        private string _lastName;
+
+        [Required]
+        [StringLength(40)]
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (value.Length < 1 || value.Length > 40) // needs more validation, ie. check for special characters
+                {
+                    throw new ArgumentException("Last name length should be 1-40 characters long");
+                }
+                _lastName = value;
+            }
+        }
+
+        private DateTime _dateOfBirth;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set
+            {
+                if (value.Year > 2023)//placeholder, will change
+                {
+                    throw new ArgumentException("You've entered an invalid date. Try again");
+                }
+                _dateOfBirth = value;
+            }
+        }
+    }
+}
