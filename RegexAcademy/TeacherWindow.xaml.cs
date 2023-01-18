45c51731dad49ff81054679ae9a9a71740f6409a
SeenaSK <104446906+SeenaSK@users.noreply.github.com>
2023-01-18 13:31:45 -0500

    Barebones of the teacher view and control

--- RegexAcademy/TeacherWindow.xaml.cs
+++ RegexAcademy/TeacherWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,34 @@ namespace RegexAcademy
         public TeacherWindow()
         {
             InitializeComponent();
+        }
+
+        private void LvTeachers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TeacherEditDeleteDlg inputDialog = new TeacherEditDeleteDlg();
+            if (inputDialog.ShowDialog() == true)
+            {
+                MessageBox.Show("Hello World");
+            }
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+            }
+
         }
     }
 }
