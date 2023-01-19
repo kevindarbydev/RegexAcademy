fa663a0a46048044af026678c63745a6d74343d5
MattReda11 <103293959+MattReda11@users.noreply.github.com>
2023-01-19 13:50:39 -0500

    Update Globals.cs

--- RegexAcademy/Globals.cs
+++ RegexAcademy/Globals.cs
@@ -10,5 +10,7 @@ namespace RegexAcademy
         {
             return toBeValidated.Any(ch => !char.IsLetterOrDigit(ch)); //iterates through a string and returns true if any char is a special char. placing this in Globals as I expect this will be useful in several places
         }
+
+        public static bool isLoggedIn = false;
     }
 }
