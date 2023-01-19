9d511c4412a63516d06828e01f2f17fa4561419b
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-01-19 13:21:26 -0500

    improved student validation

--- RegexAcademy/Globals.cs
+++ RegexAcademy/Globals.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace RegexAcademy
 {
     internal class Globals
     {
         public static RegexAcademyDbContext dbContext;
+
+        public static bool HasSpecialChars(string toBeValidated)
+        {
+            return toBeValidated.Any(ch => !char.IsLetterOrDigit(ch)); //iterates through a string and returns true if any char is a special char. placing this in Globals as I expect this will be useful in several places
+        }
     }
 }
