7c6636ecb9ff98853dc4f001ead1066100eb6cf7
SeenaSK <104446906+SeenaSK@users.noreply.github.com>
2023-01-20 10:46:22 -0500

    Adding RegexAcademyTesting project to solution explorer

---
+++ RegexAcademyTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RegexAcademyTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
