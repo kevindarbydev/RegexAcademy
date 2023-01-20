4424390ed554671fbfadb26ea4096b565ca63137
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-01-20 14:41:36 -0500

    basic search bar on 4th tab

--- RegexAcademy/Views/DataAnalyticsWindow.xaml.cs
+++ RegexAcademy/Views/DataAnalyticsWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using RegexAcademy.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
 
 namespace RegexAcademy.Views
 {
@@ -7,9 +10,29 @@ namespace RegexAcademy.Views
     /// </summary>
     public partial class DataAnalyticsWindow : Page
     {
+
+        List<object> genericMatched = new List<object>();
         public DataAnalyticsWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnSearch_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (TbxSearchBar.Text == "") return;
+            string searchString = TbxSearchBar.Text;
+
+            //iterate through Students and Teachers, only matching first name for now
+            //TODO: match by first name, last name, AND combination (All 3). some helper methods in Student class will help
+            List<Student> studentsMatched = Globals.dbContext.Students.Where(s => s.FirstName.Contains(searchString)).ToList();
+            List<Teacher> teachersMatched = Globals.dbContext.Teachers.Where(t => t.FirstName.Contains(searchString)).ToList();
+
+            genericMatched.Clear(); //clear previous results
+            genericMatched.AddRange(teachersMatched);
+            genericMatched.AddRange(studentsMatched);
+
+            LvSearchResults.ItemsSource = genericMatched;
+
         }
     }
 }
