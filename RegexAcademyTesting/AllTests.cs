3f60645d309992ab5204e06286af6407837d68f5
ChristopherPerrault <christopherperrault@hotmail.com>
2023-01-22 00:00:34 -0500

    added couple of unit tests

--- RegexAcademyTesting/AllTests.cs
+++ RegexAcademyTesting/AllTests.cs
@@ -2,22 +2,68 @@ using RegexAcademy.Models;
 
 namespace RegexAcademyTesting
 {
+
+
+    // ----------------------------------------------------
+    // tests are difficult in WPF: you can't write them for methods in the code behind, only models.
+    // ...so what that means is we can only test the few methods we have.
+    // I have seen that it's possible to test if button clicks work and other WPF elements, so I'll look into that.
+    // I have also read that seprating into a true MVVM pattern aids in testing somehow, but I'm not getting why/how it's any different than our setup.
+    // How we were taught was to put the logic in xaml.cs code-behind, not to throw it all in the class/model/object files. 
+    // Unfortunately, we may have to migrate validation + other methods into those files in order to meet the minimum 10 unit test threshold.
+    //------------------------------------------------------
+    [TestFixture]
     public class Tests
     {
+        private User _user;
+        private Teacher _teacher;
+        private Student _student;
+        private Course _course;
+
+        [SetUp]
+        public void Setup()
+        {
+            _user = new User();
+            _teacher = new Teacher();
+            _student = new Student();
+            _course = new Course();
+        }
+
+
+        // USERS
         [Test]
         public void User_Password_RegexValidation_Works()
         {
-            // ARRANGE 
-            var user1 = new User();
-            user1.Password = "password123";
+
+            _user.Password = "password123";
             bool passwordIsValid;
 
             // ACT
-            passwordIsValid = User.isPasswordValid(user1.Password, out string error);
+            passwordIsValid = User.isPasswordValid(_user.Password, out string error);
 
 
             // ASSERT
             Assert.IsFalse(passwordIsValid);
         }
+
+
+        [Test]
+        public void IsUserNameValidTest()
+        {
+            _user.Username = "RandomUser22$";
+            var result = User.isUsernameValid(_user.Username, out string error);
+            Assert.IsTrue(result, "If failed, verify Regex in Models/User.cs");
+        }
+
+
+        [Test]
+        public void IsUserNameInvalidTest()
+        {
+            _user.Username = "*Admin*";
+            var result = User.isUsernameValid(_user.Username, out string error);
+            Assert.IsFalse(result, "If failed, verify Regex in Models/User.cs");
+        }
+
+        // a series of failed test attempts were deleted here
     }
 }
