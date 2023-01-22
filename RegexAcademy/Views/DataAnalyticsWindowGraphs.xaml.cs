92447ee91bb6e653697b596db0835ca31d080992
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-01-22 08:13:03 -0500

    livecharts working, data needs massaging

--- RegexAcademy/Views/DataAnalyticsWindowGraphs.xaml.cs
+++ RegexAcademy/Views/DataAnalyticsWindowGraphs.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RegexAcademy.Views
 {
@@ -23,6 +17,64 @@ namespace RegexAcademy.Views
         public DataAnalyticsWindowGraphs()
         {
             InitializeComponent();
+
+        }
+
+        private void Page_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                ColumnSeries coursesPerWeek = new ColumnSeries
+                {
+                    Title = "Courses per day",
+                    Values = new ChartValues<int>()
+                };
+                ChartCoursesPerWeek.Series = new SeriesCollection { coursesPerWeek };
+                //ChartCoursesPerWeek.Series.Add(coursesPerWeek);
+
+                //hold the num of courses per day in dict
+                Dictionary<string, int> coursesPerDay = new Dictionary<string, int>();
+
+                //iterate over all courses, if a course is on a unique day then add to dict, else increment count
+                foreach (var course in Globals.dbContext.Courses.ToList())
+                {
+                    if (coursesPerDay.ContainsKey(course.Weekday))
+                    {
+                        coursesPerDay[course.Weekday]++;
+                    }
+                    else
+                    {
+                        coursesPerDay.Add(course.Weekday, 1);
+                    }
+                }
+                coursesPerWeek.Values = new ChartValues<int>(coursesPerDay.Values);
+
+                ChartCoursesPerWeek.AxisX.Add(new Axis
+                {
+                    Title = "Days of the Week",
+                    Labels = new List<string>(coursesPerDay.Keys)
+                });
+
+                ChartCoursesPerWeek.AxisY.Add(new Axis
+                {
+                    Title = "Number of courses",
+                    MinValue = 0,
+                });
+
+                coursesPerWeek.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                coursesPerWeek.StrokeThickness = 2;
+                ChartCoursesPerWeek.LegendLocation = LegendLocation.Right;
+
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show("Something went wrong (Courses table): " + ex.Message, "Invalid operation", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (SystemException ex)
+            {
+                MessageBox.Show("Something went wrong: " + ex.Message, "Unexpected error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
         }
     }
 }
