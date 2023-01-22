e3cc63b8547161d92ff9a6c33ca28b9df30f1ba8
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-01-22 07:14:08 -0500

    basic statistics page

--- RegexAcademy/Views/DataAnalyticsWindowStatistics.xaml.cs
+++ RegexAcademy/Views/DataAnalyticsWindowStatistics.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RegexAcademy.Views
 {
@@ -23,6 +13,19 @@ namespace RegexAcademy.Views
         public DataAnalyticsWindowStatistics()
         {
             InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            var countStudents = Globals.dbContext.Students.Count();
+            var countTeachers = Globals.dbContext.Teachers.Where(t => t.Availability == true).Count();
+            //checking for courses that are ongoing
+            var countCourses = Globals.dbContext.Courses.Where(c => (c.StartDate <= DateTime.Today && c.EndDate >= DateTime.Today)).Count();
+            // MessageBox.Show($"here {countStudents}, avail: {countTeachers} found courses: {countCourses}");
+            LblStudentsEnrolled.Content = countStudents;
+            LblTeachersAvailable.Content = countTeachers;
+            LblCoursesAvailable.Content = countCourses;
         }
     }
 }
